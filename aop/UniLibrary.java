7abb728371315eda53345488c34245780b4a3b6e
Mishka <hamann.trofimov@gmail.com>
2022-04-01 20:41:43 +0200

    Lesson_17
    @Around - what doing with Exception. Handle or throw?

--- aop/UniLibrary.java
+++ aop/UniLibrary.java
@@ -19,6 +19,7 @@ public class UniLibrary extends AbstractLibrary {
     }
 
     public String returnBook(){
+        int a = 10 / 0;
         System.out.println("returnBook: We return book");
         return "Vojna i Mir";
     }
--- aop/aspects/NewLoggingAspect.java
+++ aop/aspects/NewLoggingAspect.java
@@ -12,12 +12,19 @@ public class NewLoggingAspect {
     public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable { // proceedingJoinPoint - означает связь с target методом,
         System.out.println("aroundReturnBookLoggingAdvice: In library try return book");
 
-        long begin = System.currentTimeMillis(); // Засекаем время
-        Object targetMethodResult = proceedingJoinPoint.proceed(); //  исп.его запустим target метод
-        long end = System.currentTimeMillis(); // Засекаем время
-        
+//        long begin = System.currentTimeMillis(); // Засекаем время
+        Object targetMethodResult = null;
+        try {
+            targetMethodResult = proceedingJoinPoint.proceed(); //  исп.его запустим target метод
+        } catch (Exception e) {
+            System.out.println("aroundReturnBookLoggingAdvice: We catch some exception "+ e); // Допустим залогировали это исключение
+//            targetMethodResult = "Была ошибка метода. Не стоит обрабатывать исключение в Advice - так как это скрывает проблему из метода, а не решиет её"; // так делать не стоит
+            throw e; // Проброс исключения дальше - оптимальный вариант!
+        }
+//        long end = System.currentTimeMillis(); // Засекаем время
+
         System.out.println("aroundReturnBookLoggingAdvice: In library return book");
-        System.out.println("Method returnBook time executing: "+ (end - begin) + " millis");
+//        System.out.println("Method returnBook time executing: "+ (end - begin) + " millis");
         return targetMethodResult; // должен возвращать то же что и target (String  в нашем случае), но можно вернуть и Object
     }
 }
