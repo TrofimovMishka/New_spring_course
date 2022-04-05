0a9467067c46eb28f82cad33f035d75ecb8b8193
Mishka <hamann.trofimov@gmail.com>
2022-04-05 13:30:54 +0200

    Lesson_22
    One-to-one uni-directional associations.

---
+++ hibernate_test_2/Test1.java
@@ -0,0 +1,48 @@
+package hibernate_test_2;
+
+import hibernate_test_2.entity.Detail;
+import hibernate_test_2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration() // Создаем фабрику создания сессий
+                .configure("hibernate.cfg.xml") // Путь к файлу конфиг
+                .addAnnotatedClass(Employee.class) // Класс кот. пишем  таблицу
+                .addAnnotatedClass(Detail.class) // Класс кот. пишем  таблицу
+                .buildSessionFactory(); // метод создания фабрики
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();  // создаем сессию
+                // Add one employee
+//            Employee emp = new Employee("Liza", "Laurova", "AfterSales", 14000);
+//            Detail detail = new Detail("New York", "12346138", "ham@Gmail.com");
+//            emp.setDetail(detail);
+            // Add second employee
+//            Employee emp = new Employee("Monika", "Grid", "HR", 1000);
+//            Detail detail = new Detail("New York", "8865432", "Monika_m@Gmail.com");
+//            emp.setDetail(detail);
+
+            session.beginTransaction(); // Открываем транзакцию
+
+
+//            // Get one employee from DB
+//            Employee employee = session.get(Employee.class, 1); // with id =1
+//            System.out.println(employee.getDetail());
+//            // and save his in DB
+////            session.save(emp);
+
+
+            // Delete one employee from DB
+            Employee employee = session.get(Employee.class, 2); // with id =2
+            session.delete(employee); // details will delete too
+
+            session.getTransaction().commit(); // Закрываем транзакцию  и сохрн. измнения в БД
+        } finally {
+            session.close(); // Better way use it in catch block if we catch some Exception
+            factory.close(); // Обязательно закрываем фабрику
+        }
+    }
+}
---
+++ hibernate_test_2/entity/Employee.java
@@ -0,0 +1,98 @@
+package hibernate_test_2.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees") //Указывать имя таблицы как в MySQL называется таблица
+public class Employee { // если клас и табл. имеют одинаковые имена - то вышенаписанное можно пропустить
+    @Id //Указывает что это Primery Key
+    @GeneratedValue (strategy = GenerationType.IDENTITY) // Стратегия по созданию значения с Primery Key
+    @Column(name="id") // name -> имя столбца в таблице БД
+    private int id; // если поле называется так же как столбец то выше указаное можно не писать. Но лучше писать
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @OneToOne(cascade = CascadeType.ALL) // Выполняются операции и на связанных с ним елементов
+    @JoinColumn(name = "details_id")
+    private Detail detail;
+
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surName, String department, int salary) {
+        this.name = name;
+        this.surname = surName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    public Detail getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Detail detail) {
+        this.detail = detail;
+    }
+}
