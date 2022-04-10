738ce90c6df2e20babb37382dd7b265e6848ffd8
Mishka <hamann.trofimov@gmail.com>
2022-04-10 11:56:54 +0200

    Spring_MVC_Lesson_4
    Additional method to get info from request use @RequestParam

--- spring.mvc/src/main/java/com/company/spring/mvc/MyController.java
+++ spring.mvc/src/main/java/com/company/spring/mvc/MyController.java
@@ -3,10 +3,12 @@ package com.company.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+//@RequestMapping("/employee") // можно исп для класса
 public class MyController {
     @RequestMapping("/")
     // Когда введем в адресс браузера  <url-pattern>/</url-pattern> из web.xml то вызовем под капотом этот метод
@@ -19,7 +21,7 @@ public class MyController {
 
     }
 
-    @RequestMapping("/askDetails")
+    @RequestMapping("/askDetails") // если есть RequestMapping у класса этот URL примет вид -> /employee/askDetails
     public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
@@ -29,9 +31,20 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(HttpServletRequest request, Model model) { // request - содержит всю инфу о запросе!!!
+//        String empName = "Mr. " + request.getParameter("employeeName"); // employeeName - это имя из файла ask-epm-details-view
+//        //Добавим в модель значение кот необходимо сохранить
+//        model.addAttribute("nameAttribute", empName);//nameAttribute - имя атрибута(называй как нужно) и его значение - empName
+//        model.addAttribute("description", " - teacher");
+//
+//        return "show-emp-details-view";
+//    }
+
+    // Другой метод получения данных из запроса
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(HttpServletRequest request, Model model) { // request - содержит всю инфу о запросе!!!
-        String empName = "Mr. " + request.getParameter("employeeName"); // employeeName - это имя из файла ask-epm-details-view
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) { // employeeName - это имя из файла ask-epm-details-view
+        empName = "Mr. " + empName;
         //Добавим в модель значение кот необходимо сохранить
         model.addAttribute("nameAttribute", empName);//nameAttribute - имя атрибута(называй как нужно) и его значение - empName
         model.addAttribute("description", " - teacher");
