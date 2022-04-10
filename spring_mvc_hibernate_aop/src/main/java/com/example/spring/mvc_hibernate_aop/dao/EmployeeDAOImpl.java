d3fee96bfbbb5d8a8ae0d3a855904cea97994c05
Mishka <hamann.trofimov@gmail.com>
2022-04-10 21:25:07 +0200

    Spring_mvc_hibernate_aop_Lesson_1
    Configure and start first app.

---
+++ spring_mvc_hibernate_aop/src/main/java/com/example/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,30 @@
+package com.example.spring.mvc_hibernate_aop.dao;
+
+import com.example.spring.mvc_hibernate_aop.entity.Employee;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository // Используется для DAO
+public class EmployeeDAOImpl implements EmployeeDAO{
+
+    // Сoздает все бины описаны в contexte
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional // авто открытие и закрытие транзакци
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+
+        List<Employee> allEmployee  = session.createQuery("from Employee", Employee.class).getResultList();
+
+        return allEmployee;
+    }
+}
