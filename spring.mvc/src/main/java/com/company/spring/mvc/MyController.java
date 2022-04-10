ade4f0942b5a242e3425c0cc04a602c526de41e9
Mishka <hamann.trofimov@gmail.com>
2022-04-10 11:35:03 +0200

    Spring_MVC_Lesson_2
    Use model for save some data from user

--- spring.mvc/src/main/java/com/company/spring/mvc/MyController.java
+++ spring.mvc/src/main/java/com/company/spring/mvc/MyController.java
@@ -1,12 +1,16 @@
 package com.company.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
-    @RequestMapping("/")   // Когда введем в адресс браузера  <url-pattern>/</url-pattern> из web.xml то вызовем под капотом этот метод
-    public String showFirstView(){
+    @RequestMapping("/")
+    // Когда введем в адресс браузера  <url-pattern>/</url-pattern> из web.xml то вызовем под капотом этот метод
+    public String showFirstView() {
         //Так как мы использовали в applicationContext
         // <property name="prefix" value="/WEB-INF/view/" />
         // <property name="suffix" value=".jsp" />
@@ -16,12 +20,22 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails(){
+    public String askEmployeeDetails() {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmployeeDetails(){
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmployeeDetails(){
+    public String showEmployeeDetails(HttpServletRequest request, Model model) { // request - содержит всю инфу о запросе!!!
+        String empName = "Mr. " + request.getParameter("employeeName"); // employeeName - это имя из файла ask-epm-details-view
+        //Добавим в модель значение кот необходимо сохранить
+        model.addAttribute("nameAttribute", empName);//nameAttribute - имя атрибута(называй как нужно) и его значение - empName
+        model.addAttribute("description", " - teacher");
+
         return "show-emp-details-view";
     }
 }
--- spring.mvc/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ spring.mvc/src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -5,9 +5,11 @@
 <br>
 <br>
 <br>
-<h3> Your name: ${param.employeeName} <h3/>
-    <!-- employeeName - имя переменной из другого view кот отвечает за сохраниние данных от пользователя-->
+<%--<h3> Your name: ${param.employeeName} <h3/>--%>
+<!-- employeeName - имя переменной из другого view кот отвечает за сохраниние данных от пользователя-->
 
+<h3> Your name: ${nameAttribute} ${description}<h3/>
+    <!-- nameAttribute - это то имя которое мы присвоили в методе showEmployeeDetails() - model.addAttribute("nameAttribute", empName);-->
 
 </body>
 
